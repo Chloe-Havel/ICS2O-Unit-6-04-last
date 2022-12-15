5ff040ac3d1747e40a43873f52d48103ed6412f3
Chloe-Havel <Chloe-Havel@users.noreply.github.com>
2022-12-15 18:55:21 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -6,22 +6,29 @@
 
 "use strict"
 
-window.onload = function() {
+window.onload = function () {
   // this calculates volume of a pyramid
 
   const params = new URLSearchParams(document.location.search)
 
   // input
-  const aBase = params.get('a')
-  const bBase = params.get('b')
-  const height = params.get('h')
+  const aBase = params.get("a")
+  const bBase = params.get("b")
+  const height = params.get("h")
   console.log(aBase)
 
   // process
   const area = ((aBase + bBase) / 2) * height
-  const dimensions = "<ul>\n<li>a base = " + aBase + "</li>\n<li>b base = " + bBase + "</li>\n<li>height = " + height + "</li>\n</ul>"
+  const dimensions =
+    "<ul>\n<li>a base = " +
+    aBase +
+    "</li>\n<li>b base = " +
+    bBase +
+    "</li>\n<li>height = " +
+    height +
+    "</li>\n</ul>"
 
   // output
-  document.getElementById('dimensions').innerHTML = dimensions
-  document.getElementById('area').innerHTML = 'Area is: ' + area + ' mm²'
+  document.getElementById("dimensions").innerHTML = dimensions
+  document.getElementById("area").innerHTML = "Area is: " + area + " mm²"
 }
